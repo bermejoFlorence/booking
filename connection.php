769ffcf6fbfe06fea6ff2f72c3ed31ea33d70342
bermejoFlorence <152168210+bermejoFlorence@users.noreply.github.com>
2025-02-09 19:44:55 +0800

    changing databasename

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $database= new mysqli("localhost","root","","booking_db");
+    $database= new mysqli("localhost","u941558601_exzphotogprahy","studio_2025S","u941558601_booking_db");
     if ($database->connect_error){
         die("Connection failed:  ".$database->connect_error);
     }
