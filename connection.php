b72d8aaa6766b6ceed822b1051979cd490cadaff
bermejoFlorence <152168210+bermejoFlorence@users.noreply.github.com>
2025-04-16 22:14:50 +0800

    new database connection

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
       //connecting to database....
-    $database= new mysqli("https://exzphotography.com/","u941558601_exzphotogprahy","capstonE@2025","u941558601_finalbooking");
+      $database = new mysqli("localhost", "u941558601_exzphotogprahy", "capstonE@2025", "u941558601_finalbooking");
     if ($database->connect_error){
         die("Connection failed:  ".$database->connect_error);
     }
