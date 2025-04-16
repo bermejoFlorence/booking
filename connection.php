160305d6a3bd66d9f17eba2f457fc73707c75bb4
bermejoFlorence <152168210+bermejoFlorence@users.noreply.github.com>
2025-04-16 22:03:20 +0800

    new database connection

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
-    //connecting to database....
-    $database= new mysqli("localhost","root","","booking_db");
+      //connecting to database....
+    $database= new mysqli("localhost","u941558601_exzphotogprahy","capstonE@2025","u941558601_finalbooking");
     if ($database->connect_error){
         die("Connection failed:  ".$database->connect_error);
     }
