55e6f32f6daad76f44514ace79689e0911a9f482
bermejoFlorence <152168210+bermejoFlorence@users.noreply.github.com>
2025-04-20 19:05:09 +0800

    fixing reset password

--- connection.php
+++ connection.php
@@ -1,6 +1,8 @@
 <?php
       //connecting to database....
     $database = new mysqli("localhost", "u941558601_exzphotography", "capstonE@2025", "u941558601_finalbooking");
+   // $database = new mysqli("localhost", "u941558601_exzphotography", "capstonE@2025", "u941558601_finalbooking");
+   
     if ($database->connect_error){
         die("Connection failed:  ".$database->connect_error);
     }
