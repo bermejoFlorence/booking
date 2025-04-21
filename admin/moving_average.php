b9f7ab3c7350597a84cbf026677a73dfda5a81c2
bermejoFlorence <152168210+bermejoFlorence@users.noreply.github.com>
2025-04-21 19:27:19 +0800

    fixing layout

--- admin/moving_average.php
+++ admin/moving_average.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="../css/animations.css">  
     <link rel="stylesheet" href="../css/main.css">  
     <link rel="stylesheet" href="../css/admin.css">
-    <link rel="stylesheet" href="style.css">
+    <!-- <link rel="stylesheet" href="style.css"> -->
     <!-- FullCalendar CSS -->
 <link href="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.css" rel="stylesheet">
 <!-- FullCalendar JS -->
