c4a6ce3e58ec7a11213518130d6734042b873343
bermejoFlorence <152168210+bermejoFlorence@users.noreply.github.com>
2025-04-27 12:02:24 +0800

    fixing css

--- admin/index.php
+++ admin/index.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/animations.css">  
-    <link rel="stylesheet" href="../css/main.css">  
     <link rel="stylesheet" href="../css/admin.css">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../css/main.css"> 
     <!-- FullCalendar CSS -->
 <link href="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.css" rel="stylesheet">
 <!-- FullCalendar JS -->
