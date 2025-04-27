6e861013a3e932129cf51ce875a6a19df38e0996
bermejoFlorence <152168210+bermejoFlorence@users.noreply.github.com>
2025-04-27 12:47:47 +0800

    fixing layout

--- client/index.php
+++ client/index.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/animations.css">  
-    <link rel="stylesheet" href="../css/main.css">  
     <link rel="stylesheet" href="../css/admin.css">
     <link rel="stylesheet" href="style.css">  
+    <link rel="stylesheet" href="../css/main.css">  
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
